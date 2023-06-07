25817a3ebc4ff87b63c69698cd4570d9f3198388
Iskender Shamaldaev <sh.iskander012@gmail.com>
2023-06-07 12:53:09 +0600

    Cleaned up unnecessary files, divided into components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import Toolbar from "./components/Toolbar/Toolbar";
+import UserForm from "./components/UserForm/UserForm";
+import Users from "./components/Users/Users";
 import './App.css';
 
-function App() {
+const App = () => {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <header>
+              <Toolbar />
+          </header>
+
+          <main className="container-fluid">
+              <div className="row">
+                  <div className="col-6">
+                      Form
+                      <UserForm  />
+                  </div>
+
+                  <div className="col-6">
+                      Users
+                      <Users  />
+                  </div>
+
+              </div>
+          </main>
+      </>
   );
-}
+};
 
 export default App;
